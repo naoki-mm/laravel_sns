3789bc9765cc84b9d75eec926b173c1e4e21ac67
naoki <na@enunoMacBook-Pro.local>
2021-08-13 17:05:46 +0900

    登録機能を実装

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 // use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
+Auth::routes();
 Route::get('/', 'ArticleController@index');
